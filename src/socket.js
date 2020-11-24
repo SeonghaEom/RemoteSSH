df8ebe0448f98516927a2ade8531baa01862df1e
seongha <doubleb@kaist.ac.kr>
2020-11-24 18:46:47 +0900

    port

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,4 @@
 import io from 'socket.io-client';
-const sockets = io('https://5f074af17a4d.ngrok.io');
+// const sockets = io('https://5f074af17a4d.ngrok.io');
+const sockets = io('http://localhost:29529');
 export default sockets;
