a32ead9377411aed1cf08504f850ea98d2e9cfe8
seongha <doubleb@kaist.ac.kr>
2020-11-25 20:33:52 +0900

    fix project structure

---
+++ client/build/precache-manifest.cc1982b2b524c612e2e7150b19076244.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a060c60d7a6c9764745e84ff7b3770",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd238b77ed1a553b5a69",
+    "url": "/static/css/2.a1276a3c.chunk.css"
+  },
+  {
+    "revision": "c2604a2a8cc6a72ae3ce",
+    "url": "/static/css/main.0f694039.chunk.css"
+  },
+  {
+    "revision": "bd238b77ed1a553b5a69",
+    "url": "/static/js/2.974fa019.chunk.js"
+  },
+  {
+    "revision": "c2604a2a8cc6a72ae3ce",
+    "url": "/static/js/main.f727d4a0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "c021bbc88757eb102517688bb043156d",
+    "url": "/static/media/dialogue_1.c021bbc8.mp3"
+  },
+  {
+    "revision": "1ac02024145443a9f909d618c7651410",
+    "url": "/static/media/dialogue_2.1ac02024.mp3"
+  },
+  {
+    "revision": "25a2359dc90890728ca3bcb2c94eb655",
+    "url": "/static/media/feature-topic.25a2359d.svg"
+  },
+  {
+    "revision": "475fc97cac2ba9eda645f751f04e7e55",
+    "url": "/static/media/feature-volume.475fc97c.svg"
+  },
+  {
+    "revision": "a1ceb026ac33a422852dd733cba0b120",
+    "url": "/static/media/feature-wishlist.a1ceb026.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "25b863ba859f2ac4c8a2e53c9ad8b87b",
+    "url": "/static/media/heart.25b863ba.svg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "03a7c11b1e598d517f2977fdb56e8eb2",
+    "url": "/static/media/intro.03a7c11b.svg"
+  },
+  {
+    "revision": "27cd74ef0bb1ea69c11487967365dcee",
+    "url": "/static/media/logo.27cd74ef.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d538c167282c8fd99ead2404015451e4",
+    "url": "/static/media/logout.d538c167.svg"
+  },
+  {
+    "revision": "3266f4e67ce4fb6c56824a15c07fadc7",
+    "url": "/static/media/mic.3266f4e6.svg"
+  },
+  {
+    "revision": "4291213a10af2dfa33f48cbbab8a26ab",
+    "url": "/static/media/mute.4291213a.svg"
+  },
+  {
+    "revision": "685aac355974f8ac6280fd0f559d3dbc",
+    "url": "/static/media/other_tables.685aac35.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "000ab9d23448f385d1650355af64bb65",
+    "url": "/static/media/play-button.000ab9d2.svg"
+  },
+  {
+    "revision": "64a003799ed795e5eddb569aee444e35",
+    "url": "/static/media/thumbs-up-unclicked.64a00379.png"
+  },
+  {
+    "revision": "097977734510d987b07e73bc2a2d0761",
+    "url": "/static/media/volume.09797773.svg"
+  },
+  {
+    "revision": "4d8aae791bc0f5eacdef0c24894d6a81",
+    "url": "/static/media/wish_red.4d8aae79.svg"
+  }
+]);
--- src/reducers/sampleReducer.js
+++
@@ -1,8 +0,0 @@
-export default (state = {}, action) => {
-    switch (action.type) {
-     case 'GET_QUESTION_LIST':
-      return action.actionParameter
-     default:
-      return state
-    }
-   }
