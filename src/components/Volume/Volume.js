3df733ca1aa9e215cbb7cfe2d5cf72413a848d42
seongha <doubleb@kaist.ac.kr>
2020-11-26 21:31:23 +0900

    try awswith ssl

--- src/components/Volume/Volume.js
+++ src/components/Volume/Volume.js
@@ -78,8 +78,8 @@ const Volume = ({ display, roomId, goToScreen }) => {
     useEffect(() => {
       async function fetchData(){
          
-        await fetch('https://e92d9cbad3d1.ngrok.io/room-list')
-        // await fetch('https://3.35.238.949000/room-list')
+        // await fetch('https://e92d9cbad3d1.ngrok.io/room-list')
+        await fetch('https://3.35.238.949000/room-list')
           .then(function(response) {
             return response.json();
         }).then((json) => {
--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-const sockets = io('https://e92d9cbad3d1.ngrok.io');
+// const sockets = io('https://e92d9cbad3d1.ngrok.io');
 // const sockets = io('http://localhost:9000');
-// const sockets = io('https://3.35.238.94:9000');
+const sockets = io('https://3.35.238.94:9000');
 export default sockets;
