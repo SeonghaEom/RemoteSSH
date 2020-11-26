28a8a62b2b0f5f5ff2eea655668905c133b5626a
seongha <doubleb@kaist.ac.kr>
2020-11-27 07:07:16 +0900

    server sync with aws

--- server/index.js
+++ server/index.js
@@ -15,11 +15,11 @@ const cors = require('cors');
 //     cert: fs.readFileSync('../ryans-cert.pem')
 // };
 
-const https = require('http').createServer(app);
-const io = require('socket.io')(https);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-const https = require('https').createServer(options, app);
-const io = require('socket.io')(https);
+// const https = require('https').createServer(options, app);
+// const io = require('socket.io')(https);
 
 
 const PORT = process.env.PORT || 9000; // use 9000 port
@@ -27,8 +27,8 @@ const PORT = process.env.PORT || 9000; // use 9000 port
 // const PORT2 = 443;
 
 var corsOptions = {
-  // origin: 'https://remote-ssh.herokuapp.com',
-   origin: 'http://localhost:3000',
+  origin: 'https://remote-ssh.herokuapp.com',
+  //  origin: 'http://localhost:3000',
   credentials: true
 }
 app.use(cors(corsOptions));
